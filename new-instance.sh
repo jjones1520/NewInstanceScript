e082e5f35c53d726ec41365756fa8290efc5f2ef
Jack Jones <67930981+jjones1520@users.noreply.github.com>
2023-05-16 18:54:50 -0400

    Add files via upload

---
+++ new-instance.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get upgrade
+apt install sudo
+apt install python3
+apt install pip
+apt install curl
+apt install npm
+apt install ufw
+apt install unzip
+apt install extract
+cp -r /bin/clear /bin/1
+cp -r /bin/ls /bin/l
